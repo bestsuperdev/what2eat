e06f7a8a257af4c797a4bc3e4d2252ecf390b4bf
yonga <1143895042@qq.com>
2016-11-28 14:56:06 +0800

    update

--- routes/restaurantList.js
+++ routes/restaurantList.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var low = require('lowdb');
+var db = low('public/db/restaurant.json');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var dbValue = db.defaults()
+      .value();
+  res.send(JSON.stringify(dbValue));
 });
 
 module.exports = router;
